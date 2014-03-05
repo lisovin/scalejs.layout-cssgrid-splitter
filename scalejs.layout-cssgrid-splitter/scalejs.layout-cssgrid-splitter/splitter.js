5e51941e2cc7a0dff4020fcfd7fe3a6a95835ffb
Conor Petersen <conorpetersen@gmail.com>
2014-03-05 17:07:22 -0500

    Update splitter for new layout-cssgrid

--- scalejs.layout-cssgrid-splitter/scalejs.layout-cssgrid-splitter/splitter.js
+++ scalejs.layout-cssgrid-splitter/scalejs.layout-cssgrid-splitter/splitter.js
@@ -1,35 +1,86 @@
 ﻿/*global define, document*/
 define('scalejs.layout-cssgrid-splitter/splitter', [
     'scalejs!core',
-    'hammer'
+    'hammer',
+    'knockout'
 ], function (
     core,
     hammer
 ) {
     'use strict';
 
-    function handleDrag(element) {
-        var resizer;
+    /* example template
+    <div id="main_template">
+        <div id="main">
+            <div id="left" data-class="_left-width">Navigation</div>
+            <div id="leftSplitter" data-bind="splitter: 'final'"></div>
+            <div id="header">Header</div>
+            <div id="headerSplitter" data-bind="splitter: ''"></div>
+            <div id="content1">Content 1</div>
+            <div id="footer">Footer</div>
+        </div>
+    </div>
+    */
+
+    function handleDrag(element, mode) {
+        var resizer,
+            bgCol;
 
         function createResizer(rowOrColumn, deltaProperty, e) {
             var position = (element.currentStyle && element.currentStyle['-ms-grid-' + rowOrColumn]) ||
-                            element.getAttribute('data-ms-grid-' + rowOrColumn),
+                            core.layout.utils.safeGetStyle(element, '-ms-grid-' + rowOrColumn) ||
+                            undefined, // splitter doesnt have defined row or column. splitter is not correctly placed in your grid.
                 grid = element.parentNode,
                 definition = (grid.currentStyle && grid.currentStyle['-ms-grid-' + rowOrColumn + 's']) ||
-                              grid.getAttribute('data-ms-grid-' + rowOrColumn + 's'),
+                              core.layout.utils.safeGetStyle(grid, '-ms-grid-' + rowOrColumn + 's') ||
+                              undefined, // splitter doesnt have defined row or column. splitter is not correctly placed in your grid.
                 index,
-                dragStartDefinitions;
+                dragStartDefinitions,
+                dragSplitterPos,
+                dragSplitterDiv;
 
-            function updateDefinitions(delta) {
+
+            if (mode === 'final') {
+                dragSplitterDiv = document.createElement("div");
+                dragSplitterDiv.style.position = 'absolute';
+                dragSplitterDiv.style.height = core.layout.utils.safeGetStyle(element, 'height');
+                dragSplitterDiv.style.width = core.layout.utils.safeGetStyle(element, 'width');
+                dragSplitterDiv.className = element.className;
+                dragSplitterDiv.style.zIndex = 9999999;
+                dragSplitterDiv.style.top = element.offsetTop + 'px';
+                dragSplitterDiv.style.left = element.offsetLeft + 'px';
+                dragSplitterPos = {
+                    top: element.offsetTop + 'px',
+                    left: element.offsetLeft + 'px'
+                };
+                dragSplitterDiv.style.backgroundColor = bgCol;
+                document.body.appendChild(dragSplitterDiv);
+            }
+
+
+
+            function updateDefinitions(delta, deltaProperty) {
                 var prev = dragStartDefinitions[index - 1],
                     next = dragStartDefinitions[index + 1],
                     definitions = dragStartDefinitions.slice();
 
                 function resize(measure, delta) {
                     //console.log('--->resize: ' + left + ' by ' + delta);
-                    var value = /(\d+)/.exec(measure);
+                    var value = /(\d+)/.exec(measure),
+                        changed_measure;
                     if (value) {
-                        return (Math.max(parseInt(value, 10) + delta, 0)) + 'px';
+                        changed_measure = (Math.max(parseInt(value, 10) + delta, 0)) + 'px';
+
+                        if (mode === 'final') {
+                            if (deltaProperty === 'deltaX') {
+                                dragSplitterPos.left = changed_measure;
+                            }
+                            if (deltaProperty === 'deltaY') {
+                                dragSplitterPos.top = changed_measure;
+                            }
+                        }
+
+                        return changed_measure;
                     }
 
                     return measure;
@@ -47,16 +98,28 @@ define('scalejs.layout-cssgrid-splitter/splitter', [
             }
 
             function resize(e) {
-                var newDefinitions = updateDefinitions(e.gesture[deltaProperty]);
+                var newDefinitions = updateDefinitions(e.gesture[deltaProperty], deltaProperty),
+                    newDef;
                 if (newDefinitions) {
-                    element.parentNode.setAttribute('style', '-ms-grid-' + rowOrColumn + 's: ' + newDefinitions.join(' '));
-                    if (core.layout && core.layout.invalidate) {
+                    newDef = newDefinitions.join(' ');
+                    core.layout.utils.safeSetStyle(element.parentNode, '-ms-grid-' + rowOrColumn + 's', newDef);
+                    //element.parentNode.setAttribute('style', '-ms-grid-' + rowOrColumn + 's: ' + newDefinitions.join(' '));
+                    if (core.layout && mode !== 'final') {
                         core.layout.invalidate();
+                    }
+
+                    if (mode === 'final') {
+                        dragSplitterDiv.style.left = dragSplitterPos.left;
+                        dragSplitterDiv.style.top = dragSplitterPos.top;
                     }
                 }
             }
 
             function stop(e) {
+                if (mode === 'final') {
+                    dragSplitterDiv.parentNode.removeChild(dragSplitterDiv);
+                }
+                core.layout.invalidate();
                 resize(e);
             }
 
@@ -88,6 +151,9 @@ define('scalejs.layout-cssgrid-splitter/splitter', [
 
         return function (e) {
             switch (e.type) {
+            case 'touch':
+                bgCol = getComputedStyle(element).getPropertyValue('background-color');
+                break;
             case 'dragstart':
                 resizer = startResizing(e);
                 break;
@@ -105,7 +171,7 @@ define('scalejs.layout-cssgrid-splitter/splitter', [
     function init(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
         //element.setAttribute('data-ms-grid-column', '2');
         //element.parentNode.setAttribute('data-ms-grid-columns', 'auto 50px 1fr 1fr');
-        hammer(element).on('dragstart drag dragend', handleDrag(element));
+        hammer(element).on('dragstart drag dragend touch', handleDrag(element, valueAccessor()));
     }
     /*jslint unparam:false*/
 
